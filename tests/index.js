28c220bd763dddc035d5c06dd38446207e16b427
Mark Wylde <mark@wylde.net>
2025-08-30 21:32:22 +0100

    chore(ts): migrate to TypeScript, add build pipeline, ignore lib

--- tests/index.js
+++ tests/index.js
@@ -77,7 +77,7 @@ test('uploadDirectory should upload a directory to Bunny CDN', async (t) => {
   const options = {
     storageZoneName: STORAGE_ZONE_NAME,
     accessKey: ACCESS_KEY,
-    cleanDestination: true,
+    cleanDestination: 'simple',
     region: STORAGE_REGION
   };
 
@@ -101,4 +101,73 @@ test('uploadDirectory should upload a directory to Bunny CDN', async (t) => {
   });
 
   t.equal(response2.data, 'Test file 2 content\n', 'File 2 content should match');
+});
+
+test('uploadDirectory should recursively prune remote without deleting replacements', async (t) => {
+  t.plan(7);
+
+  const options = {
+    storageZoneName: STORAGE_ZONE_NAME,
+    accessKey: ACCESS_KEY,
+    cleanDestination: 'avoid-deletes',
+    region: STORAGE_REGION
+  };
+
+  const sourceDirectory = path.join(__dirname, 'recursiveDir');
+  const targetDirectory = 'test-upload-recursive';
+
+  // Seed remote with extra files and folders, including files that will be replaced
+  const seed = async (relPath, content) => {
+    await axios.put(
+      `https://${options.region ? options.region + "." : ""}storage.bunnycdn.com/${options.storageZoneName}/${targetDirectory}/${relPath}`,
+      content,
+      {
+        headers: {
+          'AccessKey': options.accessKey,
+          'Content-Type': 'application/octet-stream',
+        },
+      }
+    );
+  };
+
+  await seed('delete-root.txt', 'to be deleted');
+  await seed('a/keep-a.txt', 'old content that will be replaced');
+  await seed('a/delete-a.txt', 'to be deleted');
+  await seed('a/sub/delete-sub.txt', 'to be deleted');
+  await seed('b/old.txt', 'to be deleted');
+
+  await uploadDirectory(sourceDirectory, targetDirectory, options);
+
+  // Kept files should exist with new content
+  const r1 = await axios.get(`https://${options.region ? options.region + "." : ""}storage.bunnycdn.com/${options.storageZoneName}/${targetDirectory}/root.txt`, {
+    headers: { 'AccessKey': options.accessKey },
+  });
+  t.equal(r1.data, 'Root keep content\n', 'Root file content should match');
+
+  const r2 = await axios.get(`https://${options.region ? options.region + "." : ""}storage.bunnycdn.com/${options.storageZoneName}/${targetDirectory}/a/keep-a.txt`, {
+    headers: { 'AccessKey': options.accessKey },
+  });
+  t.equal(r2.data, 'Keep A content\n', 'a/keep-a.txt should be replaced');
+
+  const r3 = await axios.get(`https://${options.region ? options.region + "." : ""}storage.bunnycdn.com/${options.storageZoneName}/${targetDirectory}/a/sub/keep-sub.txt`, {
+    headers: { 'AccessKey': options.accessKey },
+  });
+  t.equal(r3.data, 'Keep Sub content\n', 'a/sub/keep-sub.txt should be uploaded');
+
+  // Deleted files should 404
+  const expect404 = async (rel) => {
+    try {
+      await axios.get(`https://${options.region ? options.region + "." : ""}storage.bunnycdn.com/${options.storageZoneName}/${targetDirectory}/${rel}`, {
+        headers: { 'AccessKey': options.accessKey },
+      });
+      t.fail(`${rel} should be deleted`);
+    } catch (err) {
+      t.equal(err.response && err.response.status, 404, `${rel} should be deleted`);
+    }
+  };
+
+  await expect404('delete-root.txt');
+  await expect404('a/delete-a.txt');
+  await expect404('a/sub/delete-sub.txt');
+  await expect404('b/old.txt');
 });
